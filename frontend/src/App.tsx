e08c264fdb52761a7b48012d290d84cc759094ee
rohith <rohithra75@gmail.com>
2024-12-02 12:36:35 -0500

    added button component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from "./components/Button";
+import { PlusIcon } from "./icons/Plusicon";
+import { ShareIcone } from "./icons/ShareIcon";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <Button variant="primary" text="Add content" startIcon={<PlusIcon />}></Button>
+      <Button variant="secondary" text="Share brain" startIcon={<ShareIcone />}></Button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
-export default App
+export default App;
